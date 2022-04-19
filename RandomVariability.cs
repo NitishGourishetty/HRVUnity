4fa6bb8482d268ee460f1819c3db82ad90e53f24
Nitish Gourishetty <59469935+NitishGourishetty@users.noreply.github.com>
2022-04-19 15:38:42 -0700

    Update RandomVariability.cs

--- RandomVariability.cs
+++ RandomVariability.cs
@@ -11,44 +11,11 @@ SampleHeartRateTrail : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ps = GetComponent<ParticleSystem>();
-        var main = ps.main;
-        var em = ps.emission;
-        var sz = ps.sizeOverLifetime;
-        var col = ps.colorOverLifetime;
-        var shape = ps.shape;
-
-        shape.shapeType = ParticleSystemShapeType.Circle;
-        shape.radiusMode = ParticleSystemShapeMultiModeValue.BurstSpread; //can alter with biofeedback
-        //var sh = ps.shape;
-        //add distance shape stuff
-        em.enabled = true;
-        shape.enabled = true;
-        //sh.enabled = true;
-        em.rateOverTime = 0.7f; // can change
-        sz.enabled = true;
-        col.enabled = true;
-        //everytime a beat happens.. #will change
-        em.SetBursts(
-            new ParticleSystem.Burst[]{
-                new ParticleSystem.Burst(2.0f, 3),
-                new ParticleSystem.Burst(10.0f, 3)
-            });
-
-        //sh.shapeType = ParticleSystemShapeType.Mesh;
-        //sh.mesh = myMesh;
-
-        AnimationCurve curve = new AnimationCurve();
-        curve.AddKey(0.0f, 0.1f);
-        curve.AddKey(0.75f, 1.0f);
-
-        sz.size = new ParticleSystem.MinMaxCurve(1.5f, curve);
-
-
-        Gradient grad = new Gradient();
-        grad.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.red, 0f), new GradientColorKey(Color.yellow, 1f) }, new GradientAlphaKey[] { new GradientAlphaKey(1f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) });
-
-        col.color = grad;
+        //Debug.Log(HRCollector.sigmoidIBI);
+        //Debug.Log(HRCollector.sigmoidHeartRate);
+        //Debug.Log(HRCollector.IBI);
+        //Debug.Log(HRCollector.heartRate);
+        
 
         //var trails = ps.trails;
         //trails.enabled = true;
@@ -63,10 +30,46 @@ SampleHeartRateTrail : MonoBehaviour
     {
         ps = GetComponent<ParticleSystem>();
         var main = ps.main;
-        main.startSize = Random.Range(1, 2); //random start size
+        var em = ps.emission;
+        var sz = ps.sizeOverLifetime;
+        var col = ps.colorOverLifetime;
+        var shape = ps.shape;
+
+        main.startSize = (float) HRCollector.sigmoidHeartRate * 3;
+        //shape.shapeType = ParticleSystemShapeType.Circle;
+        //shape.radiusMode = ParticleSystemShapeMultiModeValue.BurstSpread; //can alter with biofeedback
+        //var sh = ps.shape;
+        //add distance shape stuff
+        //em.enabled = true;
+        shape.enabled = true;
+        //sh.enabled = true;
+        em.rateOverTime = (float) HRCollector.sigmoidHeartRate; // can change
+        sz.enabled = true;
+        col.enabled = true;
+        //everytime a beat happens.. #will change
+        //em.SetBursts(
+        //    new ParticleSystem.Burst[]{
+        //        new ParticleSystem.Burst(1.0f, (float) HRCollector.sigmoidHeartRate * 10),
+        //        //new ParticleSystem.Burst(7.0f, (float) HRCollector.sigmoidIBI * 10)
+        //    });
+
+        //sh.shapeType = ParticleSystemShapeType.Mesh;
+        //sh.mesh = myMesh;
+
+        //AnimationCurve curve = new AnimationCurve();
+        //curve.AddKey(0.0f, 0.1f);
+        //curve.AddKey(0.75f, 1.0f);
+
+        //sz.size = new ParticleSystem.MinMaxCurve((float)HRCollector.sigmoidHeartRate, curve);
+
+
+        Gradient grad = new Gradient();
+        grad.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.red, 0f), new GradientColorKey(Color.yellow, 1f) }, new GradientAlphaKey[] { new GradientAlphaKey(1f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) });
+
+        col.color = grad;
+       
+        
 
 
     }
 }
-
-
